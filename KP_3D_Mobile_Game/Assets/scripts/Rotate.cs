eafc4717c79be7e3131a5b7e85f40d8bbcc28836
KP <head.iie@vnrvjiet.in>
2024-05-21 14:41:54 +0530

    Add Rotate code issue #5

---
+++ KP_3D_Mobile_Game/Assets/scripts/Rotate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotates : MonoBehaviour
+{
+    // Random rnd = new Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // int x = Random.Next(5);
+        
+        transform.Rotate(2, 0, 0);
+           
+    }
+}
